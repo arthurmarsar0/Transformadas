6035d0e9fa07352248caef3eaad325fe26607d95
PedroMonte <pedrovitor23.monte@gmail.com>
2024-10-30 16:17:17 -0300

    feat: add Color assets, add Diary view, add CarouselDayComponent, add LocationManager and Date util

---
+++ Transformadas/Transformadas/Util/Date.swift
@@ -0,0 +1,52 @@
+//
+//  Date.swift
+//  Transformadas
+//
+//  Created by Pedro Vitor de Oliveira Monte on 30/10/24.
+//
+
+import Foundation
+
+extension Date {
+    
+    var dayNumber: Int {
+        return Calendar.current.component(.day, from: self)
+    }
+    
+    var yearNumber: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    var dayOfWeekString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self)
+    }
+    
+    var monthString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.dateFormat = "MMMM"
+        return dateFormatter.string(from: self)
+    }
+    
+}
+
+func datesInCurrentMonth() -> [Date] {
+    var dates: [Date] = []
+    let today = Date.now
+
+    guard let range = Calendar.current.range(of: .day, in: .month, for: today),
+          let startOfMonth = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: today)) else {
+        return dates
+    }
+  
+    for day in range {
+        if let date = Calendar.current.date(byAdding: .day, value: day - 1, to: startOfMonth) {
+            dates.append(date)
+        }
+    }
+    
+    return dates
+}
